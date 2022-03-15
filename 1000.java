5444f88bb26dd62534b2bed0f20ebdb8b8905360
kkangwood <93994660+kkangwood@users.noreply.github.com>
2022-03-15 22:15:36 +0900

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,17 @@
+//source code
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int A;
+		int B;
+		A = input.nextInt();
+		B = input.nextInt();
+		System.out.println(A + B);
+
+
+	}
+
+}
